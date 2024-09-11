8ccec49767ebc9dcb18df4e988116295d4583666
Ossi <saurio.ossi@gmail.com>
2024-09-11 15:57:28 +0300

    Springboot projekti

---
+++ Koodi/project-tiger/src/main/java/tiikeri/project_tiger/ProjectTigerApplication.java
@@ -0,0 +1,13 @@
+package tiikeri.project_tiger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectTigerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectTigerApplication.class, args);
+	}
+
+}
---
+++ Koodi/project-tiger/src/test/java/tiikeri/project_tiger/ProjectTigerApplicationTests.java
@@ -0,0 +1,13 @@
+package tiikeri.project_tiger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectTigerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
