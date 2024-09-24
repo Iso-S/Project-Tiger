90fe798740206bd207eeed64321f91f4933bc2e8
teppohalme <teppo.halme@myy.haaga-helia.fi>
2024-09-24 08:57:38 +0300

    Repository luokat luotu

---
+++ Koodi/project-tiger/src/main/java/tiikeri/project_tiger/repository/StatusRepository.java
@@ -0,0 +1,8 @@
+package tiikeri.project_tiger.repository;
+
+import tiikeri.project_tiger.entity.Status;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface StatusRepository extends JpaRepository<Status, Long> {
+}
---
+++ Koodi/project-tiger/src/main/java/tiikeri/project_tiger/repository/TapahtumaRepository.java
@@ -0,0 +1,8 @@
+package tiikeri.project_tiger.repository;
+
+import tiikeri.project_tiger.entity.Tapahtuma;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface TapahtumaRepository extends JpaRepository<Tapahtuma, Long> {
+}
