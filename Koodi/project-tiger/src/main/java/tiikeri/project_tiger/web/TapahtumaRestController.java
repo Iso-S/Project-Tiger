30d7bb69f6a4eb4f5bf6273cbda53f6c244444ea
Ossi <saurio.ossi@gmail.com>
2024-10-01 15:56:44 +0300

    REST rajapinnan hienosäätämistä

--- Koodi/project-tiger/src/main/java/tiikeri/project_tiger/web/TapahtumaRestController.java
+++ Koodi/project-tiger/src/main/java/tiikeri/project_tiger/web/TapahtumaRestController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import tiikeri.project_tiger.entity.Tapahtuma;
@@ -17,12 +18,12 @@ public class TapahtumaRestController {
     private TapahtumaRepository TapahtumaRepository;
 
     @GetMapping("/tapahtumat")
-    public List<Tapahtuma> haeKaikkiTapahtumat() {
-        return TapahtumaRepository.findAll();
+    public @ResponseBody List<Tapahtuma> haeKaikkiTapahtumat() {
+        return (List<Tapahtuma>) TapahtumaRepository.findAll();
     }
 
     @GetMapping("/tapahtumat/{id}")
-    public Optional<Tapahtuma> haeTapahtuma(@PathVariable Long tapahtumaId) {
+    public @ResponseBody Optional<Tapahtuma> haeTapahtuma(@PathVariable("id") Long tapahtumaId) {
         return TapahtumaRepository.findById(tapahtumaId);
     }
 }
